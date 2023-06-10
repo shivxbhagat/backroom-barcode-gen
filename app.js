c5c3cdb465734fd40e30dc823194811e75ff2a93
Shiv <shivbhagat@icloud.com>
2023-06-10 06:13:34 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const generateBarcode = () => {
+  let text = document.getElementById("text").value;
+
+  if (text.trim().length === 0) {
+    alert("Please enter a valid text.");
+    return;
+  }
+
+  let numbers = text.split("/"); 
+
+
+  let convertedText = "A"+numbers[0]+ "R" + numbers[1] + "S" + numbers[2] + "00100";
+  console.log(convertedText);
+  JsBarcode("#barcode", convertedText, {
+    format: "CODE128",
+    lineColor: "#000",
+    width: 2,
+    height: 100,
+    displayValue: true,
+    text: text,
+  });
+};
+
+
+document.getElementById("btn").addEventListener("click", generateBarcode);
+
+
+
+
+
